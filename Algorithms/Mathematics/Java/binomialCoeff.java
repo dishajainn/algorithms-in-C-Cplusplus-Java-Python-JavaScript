d804fb8dc131be0d467d2e11f9a4fa0fa08f29a4
Disha Jain <jainguddu334@gmail.com>
2019-10-02 13:29:37 +0530

    add math algos in java

---
+++ Algorithms/Mathematics/Java/binomialCoeff.java
@@ -0,0 +1,24 @@
+import java.util.Scanner; 
+
+class binomialCoeff { 
+	
+	// Returns value of Binomial Coefficient C(n, k) 
+	static int binomialCoefficient(int n, int k) 
+	{ 
+		if (k == 0 || k == n) 
+			return 1; 
+		
+		return binomialCoefficient(n - 1, k - 1) + 
+					binomialCoefficient(n - 1, k); 
+	} 
+	
+	public static void main(String[] args) 
+	{ 
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int k = scn.nextInt();
+		int ans = binomialCoefficient(n, k);
+		System.out.println("Value of " + n + "C" + k + ": " + ans);
+	} 
+} 
+
